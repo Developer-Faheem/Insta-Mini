c6821a93b6aaa5de17d64e956430673f971cb20a
faheem abbas <dev.faheemabbas@gmail.com>
2023-02-20 21:37:12 +0500

    done with the search screen

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_c/screens/add_post_screen.dart';
 import 'package:instagram_c/screens/feed_screen.dart';
+import 'package:instagram_c/screens/search_screen.dart';
 
 var webScreenSize = 600;
 
 var home = [
   FeedScreen(),
-  Text("search"),
+  SearchScreen(),
   AddPostScreen(),
   Text("faviourite"),
   Text("person")
